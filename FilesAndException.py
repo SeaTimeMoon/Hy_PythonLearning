f63557b859d0d2e19e7c8cfc3d3e78a76c3cfa54
Carl Hu <hy19900815work@126.com>
2017-09-02 10:08:24 +0800

    exception add

--- FilesAndException.py
+++ FilesAndException.py
@@ -63,3 +63,54 @@ with open(filename2, 'a') as file_object7:
     file_object7.write("I love creating apps that can run in a browser.\n")
 
 
+#Exception
+
+#ZeroDivisionError
+# print("Give me two numbers, and I'll divide them.")
+# print("Enter 'q' to quit.")
+#
+# while True:
+#     first_number = input("\nFirst number: ")
+#     if first_number == 'q':
+#         break
+#     second_number = input("Second number: ")
+#     try:
+#         answer = int(first_number)/int(second_number)
+#     except ZeroDivisionError:
+#         print("You can't divide by 0!")
+#     else:
+#         print(answer)
+
+
+# FileNotFoundError
+
+filename = 'alice.txt'
+try:
+    with open(filename) as f_obj:
+        contents = f_obj.read()
+except FileNotFoundError:
+    msg = "Sorry, the file " + filename + " does not exist."
+    print(msg+'\n')
+
+
+# Words Analyse
+
+def count_words(file_name_path):
+    """calculate the words count in a file"""
+    try:
+        with open(file_name_path) as f_obj10:
+            contents = f_obj10.read()
+    except FileNotFoundError:
+        # msg = "Sorry, the file " + filenamepath + " does not exist."
+        # print(msg)
+        pass
+    else:
+        # calculate words count in file
+        words = contents.split()
+        num_words = len(words)
+        print("The file " + file_name_path + " has about " + str(num_words) + " words.")
+
+file_names = ['A Creature of the Night.txt','Alice in wonderland','David Copperfield.txt','Jane Eyre.txt','Aesop’s Fables.txt'
+              ,'a tale of two cities.txt']
+for filename in file_names:
+    count_words(filename)
